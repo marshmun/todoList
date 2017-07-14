40da8b5211867d07e95809c7ed2356b9ccba6993
Marshall Munoz <marshmun@gmail.com>
2017-07-14 11:18:15 -0500

    deleteing and updating now possible

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var mylist = [
     "eat all of the hamburgers"
 ]
 
-app.use('/', express.static('./views'))
+app.use('/', express.static('./public'))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -23,7 +23,6 @@ app.set('views', './views');
 
 app.get("/", function (req, res) {
     models.todo.findAll().then(function (currentTodo) {
-        // res.send(currentTodo);
         res.render("index", { todos: currentTodo });
 
     }).catch(function (err) {
@@ -31,34 +30,44 @@ app.get("/", function (req, res) {
     })
 });
 
+app.get('/update/:idrs', (req, res) => {
+    models.todo.findOne({ id: req.params.id })
+        .then(singleTodo => {
+            res.render('update', { updatethis: singleTodo })
+        })
+})
+
+
+
+app.post('/update/:id', (req, res) => {
+    models.todo.update({ title: req.body.title }, { where: { id: req.params.id } })
+        .then(updatedtodo => {
+            res.redirect('/')
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
+})
 
 app.post("/", function (req, res) {
     var taskTitle = req.body.taskTitle;
-    console.log('input: ', taskTitle);
-
-    todos.push(req.body.taskTitle);
-    // res.redirect('/');
     var newTodo = models.todo.build({ title: taskTitle });
     newTodo
         .save()
         .then(function (savedInput) {
-            // res.send(savedInput)
-            // redirecthome
             res.redirect('/')
         }).catch(function (err) {
             res.status(500).send(err);
         })
 })
 
-app.delete('/', function (req, res) {
-    models.todo.destroy({ where: { id: req.params.id } })
-        .then(function () {
-            res.send("deleted User")
-        })
-        .catch(function (err) {
-            res.status(500).send(err);
-        })
-})
+app.post("/delete-post", (req, res) => {
+    models.todo.destroy({ where: { id: req.body.id } }).then(() => {
+        return res.redirect('/')
+    }).catch(err => {
+        res.send('error')
+    })
+});
 
 
 app.listen(port, function () {
